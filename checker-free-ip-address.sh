cf6c3377a346ae86884743bd5e7e0f73d0de6a8d
Johnimony <61118899+Georg-get@users.noreply.github.com>
2021-11-22 21:57:32 +0300

    bash-1-2
    Added a loop ,that looks for free IP-addresses in the local network

--- checker-free-ip-address.sh
+++ checker-free-ip-address.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-ping 192.168.10.101 -c1 | grep icmp_seq | awk '{print $1}'
+  NET=${NET:-$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d '/' | cut -f 1-3 -d .)}
+for ip in {1..254..1}
+do
+status=`ping -w1 -c1 ${NET}.${ip}`
+         if [[ $? == 1 ]];then
+                 echo "${NET}.${ip} свободен"
+         fi;
+done
